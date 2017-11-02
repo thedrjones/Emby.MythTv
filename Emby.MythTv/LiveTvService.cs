30b6450e21feb1d8647a8e5c7caf29757d1f914f
Tom Andrews <tom@andrewsmail.co.uk>
2017-11-02 15:32:01 +0000

    Move ScheduleDirect class into Model

--- Emby.MythTv/LiveTvService.cs
+++ Emby.MythTv/LiveTvService.cs
@@ -1,6 +1,7 @@
 ﻿using Emby.MythTv.Helpers;
 using Emby.MythTv.Responses;
 using Emby.MythTv.Protocol;
+using Emby.MythTv.Model;
 using MediaBrowser.Common.Extensions;
 using MediaBrowser.Common.Net;
 using MediaBrowser.Controller.Drawing;
@@ -881,34 +882,6 @@ namespace Emby.MythTv
             throw new NotImplementedException();
         }
 
-        public class ScheduleDirect
-        {
-            public class Caption
-            {
-                public string content { get; set; }
-                public string lang { get; set; }
-            }
-
-            public class ImageData
-            {
-                public string width { get; set; }
-                public string height { get; set; }
-                public string uri { get; set; }
-                public string size { get; set; }
-                public string aspect { get; set; }
-                public string category { get; set; }
-                public string text { get; set; }
-                public string primary { get; set; }
-                public string tier { get; set; }
-                public Caption caption { get; set; }
-            }
-
-            public class ShowImages
-            {
-                public string programID { get; set; }
-                public List<ImageData> data { get; set; }
-            }
-        }
     }
 
 }
